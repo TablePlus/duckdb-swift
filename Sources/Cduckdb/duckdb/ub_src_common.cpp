b816b56ec70d289496bbbb447f957ad321b3735d
Henry <duchuykun@gmail.com>
2026-06-08 00:49:01 +0700

    Fix import.

--- Sources/Cduckdb/duckdb/ub_src_common.cpp
+++ Sources/Cduckdb/duckdb/ub_src_common.cpp
@@ -1,6 +1,12 @@
 #line 0 "src/common/allocator/allocator.cpp"
 #include "src/common/allocator/allocator.cpp"
 
+#line 0 "src/common/allocator/allocator_standard.cpp"
+#include "src/common/allocator/allocator_standard.cpp"
+
+#line 0 "src/common/allocator/allocator_jemalloc.cpp"
+#include "src/common/allocator/allocator_jemalloc.cpp"
+
 #line 0 "src/common/assert.cpp"
 #include "src/common/assert.cpp"
 
